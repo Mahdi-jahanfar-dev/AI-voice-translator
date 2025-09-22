ccd87f655a2da9c71ff523aa69941a25ecbdada1
Mahdi-jahanfar-dev <mahdi.jahanfar01@gmail.com>
2025-09-22 16:18:38 +0330

    feat: added project models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class VoiceDetail(models.Model):   
+    title = models.CharField(max_length=200)
+    voice = models.FileField(upload_to="audio", null=True, blank=True)
+    voice_text = models.TextField()
+    
+
+class TranslatedText(models.Model):
+    voice = models.ForeignKey(VoiceDetail, on_delete=models.CASCADE, related_name="translates")
+    translated_text = models.TextField()
+    translated_language = models.CharField(max_length=100)
